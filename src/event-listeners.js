29e01c89ee39b0984fde5bae93259c5ae9a29b61
Isaac Akinsanya <IsaacAki247@gmail.com>
2025-09-03 19:09:15 +0100

    Moved page elements to event-listeners.js and updated variable names

--- src/event-listeners.js
+++ src/event-listeners.js
@@ -1,48 +1,106 @@
 import { projects, createProject, findProjectByName, findProjectById } from "./projects";
-import { pageElements } from "./page-elements";
 import { createTask } from "./todo";
 
-const el = new pageElements();
+let currentProject;
+
+const container = document.querySelector('.container');
+const sidebar = document.querySelector('.sidebar');
+const tasksNav = document.querySelector('.tasks-nav');
+const projectsNav = document.querySelector('.projects-nav');
+const projNav = document.querySelectorAll('.projects-nav');
+const mainContent = document.querySelector('.main-content');
+const header = document.querySelector('.header');
+const tasksContainer = document.querySelector('.tasks');
+
+// PROJECTS
+const projectName = document.querySelector('#project-name');
+const newProjectBtn = document.querySelector('.new-project-btn');
+const submitProjectBtn = document.querySelector('.submit-project-btn');
+const projectDialog = document.querySelector('.project-dialog');
+const closeProjectDialog = document.querySelector('.close-project-dialog');
+
+// TASKS
+const newTaskBtn = document.querySelector('.new-task-btn');
+const submitTaskBtn = document.querySelector('.submit-task-btn');
+const taskDialog = document.querySelector('.task-dialog');
+const closeTaskDialog = document.querySelector('.close-task-dialog');
+const editTask = document.querySelector('.edit-task');
+const deleteTask = document.querySelector('.delete-task');
+const cards = document.querySelector('.cards');
+
+// FORM
+const titleInput = document.querySelector('#title');
+const radioLow = document.querySelector('#radio-low');
+const radioMedium = document.querySelector('#radio-medium');
+const radioHigh = document.querySelector('#radio-high');
+const dateInput = document.querySelector('#date');
+const dropdown = document.querySelector('#projects-dropdown');
+const notesInput = document.querySelector('#notes');
 
 export const listeners = (() => {
 
     // TASKS
-    el.newTaskBtn.addEventListener('click', () => {
-        el.taskDialog.showModal();
+    newTaskBtn.addEventListener('click', () => {
+        taskDialog.showModal();
     });
 
-    el.submitTaskBtn.addEventListener('click', () => {
+    submitTaskBtn.addEventListener('click', () => {
         storeTaskInfo();
-        el.taskDialog.close();
+        taskDialog.close();
         resetForm();
     });
 
-    el.closeTaskDialog.addEventListener('click', () => {
-        el.taskDialog.close();
+    closeTaskDialog.addEventListener('click', () => {
+        taskDialog.close();
         resetForm();
     });
 
     // PROJECTS
-    el.newProjectBtn.addEventListener('click', () => {
-        el.projectDialog.showModal();
+    newProjectBtn.addEventListener('click', () => {
+        projectDialog.showModal();
     });
     
-    el.submitProjectBtn.addEventListener('click', () => {
+    submitProjectBtn.addEventListener('click', () => {
         storeProjectInfo(projects);
-        el.projectName.value = '';
-        el.projectDialog.close();
+        projectName.value = '';
+        projectDialog.close();
     });
 
-    el.closeProjectDialog.addEventListener('click', () => {
-       el.projectDialog.close();
-       el.projectName.value = '';
+    closeProjectDialog.addEventListener('click', () => {
+       projectDialog.close();
+       projectName.value = '';
     });
 
-    for (const button of el.projNav) {
+    for (const button of projNav) {
         button.addEventListener('click', (e) => {
             displayTasks(e);
         })
     }
+
+    editTask.addEventListener('click', (e) => {
+        taskDialog.showModal();
+
+        const proj = findProjectById(currentProject);
+        const taskId = e.target.dataset.id;
+        const task = proj.findTask(taskId);
+
+        titleInput.value = task.title;
+        dateInput.value = task.dueDate;
+        dropdown.options = currentProject;
+        notesInput.value = task.notes
+
+        // proj.editTask(taskId, title, priority, dueDate, notes);
+
+        // const title = titleInput.value;
+        // const priority = selectPriority();
+        // const dueDate = dateInput.value;
+        // const selectedProject = dropdown.options[dropdown.selectedIndex].dataset.id;
+        // const notes = notesInput.value;
+
+        // if (title.trim().length === 0 || dueDate.trim().length === 0) {
+        //     return;
+        // }
+    });
 
 })();
 
@@ -53,22 +111,22 @@ export const displayProjects = () => {
         const btn = document.createElement('button');
         btn.dataset.id = project.id;
         btn.textContent = project.projectName;
-        el.projectsNav.appendChild(btn);
+        projectsNav.appendChild(btn);
 
         newOption.value = project.projectName;
         newOption.text = project.projectName;
         newOption.dataset.id = project.id;
     });
 
-    el.dropdown.add(newOption);
+    dropdown.add(newOption);
 }
 
 function storeTaskInfo() {
-    const title = el.titleInput.value;
+    const title = titleInput.value;
     const priority = selectPriority();
-    const dueDate = el.dateInput.value;
-    const selectedProject = el.dropdown.options[el.dropdown.selectedIndex].dataset.id;
-    const notes = el.notesInput.value;
+    const dueDate = dateInput.value;
+    const selectedProject = dropdown.options[dropdown.selectedIndex].dataset.id;
+    const notes = notesInput.value;
 
     if (title.trim().length === 0 || dueDate.trim().length === 0) {
         return;
@@ -76,45 +134,45 @@ function storeTaskInfo() {
 
     const task = createTask(title, priority, dueDate, notes);
     const project = findProjectById(selectedProject);
-    const allProjects = findProjectByName('All Projects');
-    if (project === allProjects) {
-        allProjects.addTask(task);
+    const allTasks = findProjectByName('All Tasks');
+    if (project === allTasks) {
+        allTasks.addTask(task);
     } else {
         project.addTask(task);
-        allProjects.addTask(task);
+        allTasks.addTask(task);
     }
 }
 
 function storeProjectInfo() {
-    const projectName = el.projectName.value;
+    const name = projectName.value;
 
-    if (projectName.trim().length === 0) {
+    if (name.trim().length === 0) {
         return;
     }
 
-    createProject(projectName);
-    el.projectsNav.textContent = '';
+    createProject(name);
+    projectsNav.textContent = '';
     displayProjects();
 }
 
 // TODO: GET THE REMAINING VALUES AND CLEAR THEM
 const resetForm = () => {
-    el.titleInput.value = null;
-    el.radioLow.checked = false;
-    el.radioMedium.checked = false;
-    el.radioHigh.checked = false;
-    el.dateInput.value = null;
-    el.dropdown.value = 'All Projects';
-    el.notesInput.value = null;
+    titleInput.value = null;
+    radioLow.checked = false;
+    radioMedium.checked = false;
+    radioHigh.checked = false;
+    dateInput.value = null;
+    dropdown.value = 'All Tasks';
+    notesInput.value = null;
 }
 
 const selectPriority = () => {
-    if (el.radioLow.checked === true) {
-        return el.radioLow.value;
-    } else if (el.radioMedium.checked === true) {
-        return el.radioMedium.value;
+    if (radioLow.checked === true) {
+        return radioLow.value;
+    } else if (radioMedium.checked === true) {
+        return radioMedium.value;
     } else {
-        return el.radioHigh.value;
+        return radioHigh.value;
     }
 }
 
@@ -127,24 +185,37 @@ const showTasks = (task) => {
     let priority = document.createElement('p');
     let dueDate = document.createElement('p');
     let notes = document.createElement('p');
+    let editBtn = document.createElement('button');
+    let deleteBtn = document.createElement('button');
 
     title.textContent = task.title;
     priority.textContent = task.priority;
     dueDate.textContent = task.dueDate;
     notes.textContent = task.notes;
+    editBtn.textContent = 'Edit';
+    deleteBtn.textContent = 'Delete';
+
+    editBtn.classList.add('edit-task');
+    editBtn.dataset.id = task.id;
+
+    deleteBtn.classList.add('delete-task');
+    deleteBtn.dataset.id = task.id;
 
     div.append(title);
     div.append(priority);
     div.append(dueDate);
     div.append(notes);
+    div.append(editBtn);
+    div.append(deleteBtn);
 
-    el.tasksContainer.appendChild(div);
+    tasksContainer.appendChild(div);
 }
 
 const displayTasks = (e) => {
-    el.tasksContainer.textContent = '';
+    tasksContainer.textContent = '';
 
     const item = e.target.dataset.id;
+    currentProject = item;
     const project = findProjectById(item);
 
     project.todos.forEach(task => showTasks(task));
